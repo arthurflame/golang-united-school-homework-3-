ab5cd5ab6b95031f9daa233ed3b8cee748ee928d
arthurflame <artgerash@gmail.com>
2022-05-06 16:15:30 +0300

    first try?

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,6 +1,14 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-	//Place your code here
-	return
+	var sum float32
+	var elements float32
+
+	for i := 0; i < len(input); i++ {
+		if input[i] != 0 {
+			sum += input[i]
+			elements++
+		}
+	}
+	return sum / elements
 }
--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,9 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	//Place your code here
-	return
+	for i := len(input)/2 - 1; i >= 0; i-- {
+		opp := len(input) - 1 - i
+		input[i], input[opp] = input[opp], input[i]
+	}
+	return input
 }
